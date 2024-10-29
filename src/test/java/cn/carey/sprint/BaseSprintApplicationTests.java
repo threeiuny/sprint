b5cd4b21c2e9263a365b85a8866da3c05c43c2ff
Zeno Mengjuan <129027671+threeiuny@users.noreply.github.com>
2024-10-29 14:12:04 +0800

    init project
    init project

---
+++ src/test/java/cn/carey/sprint/BaseSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.carey.sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
