21046cc041cae24fee2cb314ee3b62d9202e4fe0
Mr.Chen <644753271@qq.com>
2024-11-21 18:00:08 +0800

    添加条件注解以确保在存在相关Bean时加载JPA配置，优化JPA Repository和事务管理配置

--- base-spring-datasource/base-sprint-datasource-jpa/src/main/java/cn/carey/jpa/config/JpaConfig.java
+++ base-spring-datasource/base-sprint-datasource-jpa/src/main/java/cn/carey/jpa/config/JpaConfig.java
@@ -1,6 +1,8 @@
 package cn.carey.jpa.config;
 
 import cn.carey.jpa.config.properties.JpaProperties;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(JpaProperties.class)
+@ConditionalOnBean(DataSource.class)
 public class JpaConfig {
     
     /**
--- base-spring-datasource/base-sprint-datasource-jpa/src/main/java/cn/carey/jpa/config/JpaRepositoryConfig.java
+++ base-spring-datasource/base-sprint-datasource-jpa/src/main/java/cn/carey/jpa/config/JpaRepositoryConfig.java
@@ -1,13 +1,21 @@
 package cn.carey.jpa.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import jakarta.persistence.EntityManagerFactory;
 
 /**
  * JPA Repositories配置类
  * 用于启用JPA Repositories功能并指定扫描路径
  */
 @Configuration
+@ConditionalOnBean({
+    EntityManagerFactory.class,
+    PlatformTransactionManager.class
+})
 @EnableJpaRepositories(
     basePackages = "${spring.jpa.repository.packages}",
     entityManagerFactoryRef = "entityManagerFactory",
